06a5616a97a30d65c881632b4da6e43262fb6d34
Brayan Ciro <ciroescuderobrayan@gmail.com>
2026-05-12 14:43:58 -0500

    Agregar entidad UserPogress

---
+++ src/main/java/org/sparkfi/model/UserProgress.java
@@ -0,0 +1,75 @@
+package org.sparkfi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_progress")
+public class UserProgress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer progressPercentage;
+    private Boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    public UserProgress() {
+    }
+
+    public UserProgress(User user, Course course, Integer progressPercentage, Boolean completed) {
+        this.user = user;
+        this.course = course;
+        this.progressPercentage = progressPercentage;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getProgressPercentage() {
+        return progressPercentage;
+    }
+
+    public void setProgressPercentage(Integer progressPercentage) {
+        this.progressPercentage = progressPercentage;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
